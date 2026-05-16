d4e1ec4d79a7e97526730d37e41b9d93ac7a6a39
Md. Mahadee Hasan Abir <37393027+mahade10@users.noreply.github.com>
2026-05-16 09:15:21 +0600

    add user seeds

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -7,3 +7,8 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+User.create!(first_name: "Abir", 
+ last_name: "Hossain",
+ email: "abir.cse.ru@gmail.com",
+ password: "asdasd123")
